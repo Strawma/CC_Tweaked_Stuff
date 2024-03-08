a95945edde5407ba99afcf1aed6338eba431e908
Ramis <111586342+Strawma@users.noreply.github.com>
2024-03-08 20:00:23 +0000

    revised code cleanups

--- Scriptor_Teleporter/Sender/startup.lua
+++ Scriptor_Teleporter/Sender/startup.lua
@@ -1,3 +1,27 @@
-os.loadAPI("generic_startup.lua")
 local mainUrl = "https://raw.githubusercontent.com/Strawma/CC_Tweaked_Stuff/main/Scriptor_Teleporter/Sender/main"
-generic_startup.start(mainUrl)
+
+---------------------------------------------------
+local mainFile = "main.lua"
+
+local apiUrl = "https://raw.githubusercontent.com/Strawma/CC_Tweaked_Stuff/main/strawma_api.lua"
+local apiFile = "strawma_api.lua"
+
+local function download(url, file)
+    if fs.exists(file) then
+        fs.delete(file)
+    end
+    shell.run("wget", url, file)
+end
+
+download(apiUrl, apiFile)
+os.loadAPI(apiFile)
+
+local function setProtocol()
+    local fileName = "PROTOCOL.txt"
+    strawma_api.tryReadWriteFile(fileName, "Enter protocol: ")
+end
+
+setProtocol()
+
+download(mainUrl, mainFile)
+shell.run(mainFile)
--- generic_startup.lua
+++ generic_startup.lua
@@ -1,27 +1,27 @@
+local mainUrl -- URL Goes Here
 
-function start(mainUrl)
-    local mainFile = "main.lua"
+---------------------------------------------------
+local mainFile = "main.lua"
 
-    local apiUrl = "https://raw.githubusercontent.com/Strawma/CC_Tweaked_Stuff/main/strawma_api.lua"
-    local apiFile = "strawma_api.lua"
-    
-    local function download(url, file)
-        if fs.exists(file) then
-            fs.delete(file)
-        end
-        shell.run("wget", url, file)
+local apiUrl = "https://raw.githubusercontent.com/Strawma/CC_Tweaked_Stuff/main/strawma_api.lua"
+local apiFile = "strawma_api.lua"
+
+local function download(url, file)
+    if fs.exists(file) then
+        fs.delete(file)
     end
-    
-    download(apiUrl, apiFile)
-    os.loadAPI(apiFile)
-    
-    local function setProtocol()
-        local fileName = "PROTOCOL.txt"
-        strawma_api.tryReadWriteFile(fileName, "Enter protocol: ")
-    end
-    
-    setProtocol()
-    
-    download(mainUrl, mainFile)
-    shell.run(mainFile)
+    shell.run("wget", url, file)
 end
+
+download(apiUrl, apiFile)
+os.loadAPI(apiFile)
+
+local function setProtocol()
+    local fileName = "PROTOCOL.txt"
+    strawma_api.tryReadWriteFile(fileName, "Enter protocol: ")
+end
+
+setProtocol()
+
+download(mainUrl, mainFile)
+shell.run(mainFile)
