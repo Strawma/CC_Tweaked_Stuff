187542c494a383a7d71d57d08e68178b2f1f62c5
Ramis <111586342+Strawma@users.noreply.github.com>
2024-03-08 17:26:34 +0000

    added a method of creating and saving a channel number

--- startup.lua
+++ startup.lua
@@ -4,6 +4,24 @@
 local url = "" --url goes here
 local file = "main.lua"
 
+local function getChannel()
+    local fileName = "CHANNEL.txt"
+    if fs.exists(file) then
+        local file = fs.open(fileName, "r")
+        channel = file.readLine()
+        file.close()
+    else
+        local file = fs.open(fileName, "w")
+        repeat
+            write("Enter the channel number: ")
+            channel = read()
+        until tonumber(channel)
+        file.write(channel)
+        file.close()
+    end
+    return tonumber(channel)
+end
+
 local function download(url, file)
     if fs.exists(file) then
         fs.delete(file)
