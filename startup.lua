be0a87cd88bebf1c880d880a9217f3e94272f6c2
Ramis <111586342+Strawma@users.noreply.github.com>
2024-03-08 18:17:58 +0000

    forgot to save api under new name

--- startup.lua
+++ startup.lua
@@ -5,7 +5,7 @@ local url = "" --url goes here
 local file = "main.lua"
 
 local apiUrl = "https://raw.githubusercontent.com/Strawma/CC_Tweaked_Stuff/main/strawma_api.lua"
-local apiFile = "strawma-api.lua"
+local apiFile = "strawma_api.lua"
 
 local function setChannel()
     local fileName = "CHANNEL.txt"
