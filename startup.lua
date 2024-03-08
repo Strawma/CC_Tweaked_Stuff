71677f30273a443d7929eb7f7ce59110ca85fe7c
Ramis <111586342+Strawma@users.noreply.github.com>
2024-03-08 18:15:35 +0000

    now uses correct api path

--- startup.lua
+++ startup.lua
@@ -4,7 +4,7 @@
 local url = "" --url goes here
 local file = "main.lua"
 
-local apiUrl = "https://raw.githubusercontent.com/Strawma/CC_Tweaked_Stuff/main/strawma-api.lua"
+local apiUrl = "https://raw.githubusercontent.com/Strawma/CC_Tweaked_Stuff/main/strawma_api.lua"
 local apiFile = "strawma-api.lua"
 
 local function setChannel()
