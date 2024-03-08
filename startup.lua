616005849a81a9bf5c9bb80b8a8ea1db9f310d57
Ramis <111586342+Strawma@users.noreply.github.com>
2024-03-08 17:51:04 +0000

    bug fix #1

--- startup.lua
+++ startup.lua
@@ -10,7 +10,7 @@ local apiFile = "strawma-api.lua"
 local function setChannel()
     local fileName = "CHANNEL.txt"
     local validChannel = false
-    if fs.exists(file) then
+    if fs.exists(fileName) then
         local file = fs.open(fileName, "r")
         local channel = file.readLine()
         file.close()
