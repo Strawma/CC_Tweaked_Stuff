3bf6c706df4ee9f88f549935e2fcd86029a94d54
Ramis <111586342+Strawma@users.noreply.github.com>
2024-03-09 18:34:11 +0000

    trying to run location pings and input request simultaneously

--- Scriptor_Teleporter/Sender/main.lua
+++ Scriptor_Teleporter/Sender/main.lua
@@ -2,48 +2,57 @@ os.loadAPI("strawma_api.lua")
 local PROTOCOL = strawma_api.getProtocol()
 local MODEM = "back"
 
+local availableLocations = "Searching for locations..."
+
 rednet.open(MODEM)
 
 local function teleportRequest(location)
     rednet.broadcast("tp", PROTOCOL .. location)
 end
 
-local function discoverRequest()
-    strawma_api.refresh()
-    print("Discovering locations...")
-    local timerID = os.startTimer(3)
-    local found = {}
-    rednet.broadcast("discover", PROTOCOL)
-    while true do
-        local event, id, name, protocol = os.pullEvent()
-        if event == "timer" and id == timerID then
-            break
-        elseif event == "rednet_message" and protocol == PROTOCOL .. "discovery_response" then
-            table.insert(found, name)
-        end
-    end
-    local msg
+local function constructLocations(found)
     if #found == 0 then
         return "No locations found"
     else
-        msg = "Locations found:"
+        local msg = "Locations found:"
         for i, name in ipairs(found) do
             msg = msg .. "\n" .. i .. ": " .. name
         end
+        return msg
     end
-    msg = msg .. "\n"
-    return msg
 end
 
-local displayMsg = discoverRequest()
-while true do
-    strawma_api.refresh()
-    print(displayMsg)
-
-    write("Enter a location: ")
-    local location = read()
-    if location == "exit" then
-        break
+local function discoverRequest()
+    while true do
+        local timerID = os.startTimer(10) 
+        local found = {}
+        rednet.broadcast("discover", PROTOCOL)
+        while true do
+            local event, id, name, protocol = os.pullEvent()
+            if event == "timer" and id == timerID then
+                break
+            elseif event == "rednet_message" and protocol == PROTOCOL .. "discovery_response" then
+                table.insert(found, name)
+            end
+        end
+        availableLocations = constructLocations(found)
     end
-    teleportRequest(location)
 end
+
+local function run()
+    while true do
+        strawma_api.refresh()
+        print(availableLocations .. "\n")
+    
+        write("Enter a location: ")
+        local location = read()
+
+        teleportRequest(location)
+    end
+end
+
+local function runInParallel()
+    parallel.waitForAny(discoverRequest, run)
+end
+
+runInParallel()
